5027b08140836c19816f14941b4f037bc5e0ee5e
Dylan Torres Cruz <dylan@programmer.city>
2019-08-20 10:45:24 -0500

    Add library ImagePicker

--- app/containers/home/Profile.js
+++ app/containers/home/Profile.js
@@ -1,20 +1,47 @@
 import React,{Component} from 'react'
-import {StyleSheet,View} from 'react-native'
+import {StyleSheet,View,TouchableOpacity} from 'react-native'
 import {Container,Content,Text, Header, Thumbnail} from 'native-base'
 import DYHeader from '../../components/DYHeader';
-
+import ImagePicker from 'react-native-image-picker';
 
 export default class Profile extends Component{
     static navigationOptions = {
         header: null
       };
 
+      state={
+          avatarSource:{uri:'https://vignette.wikia.nocookie.net/yugiohenespanol/images/c/c8/Jaden_Yuki_original.jpg/revision/latest?cb=20160215221041&path-prefix=es'}
+      }
       drawerOpen=()=>{
         this.props.navigation.openDrawer()
       }
-     
+     onPress=()=>{
+        ImagePicker.showImagePicker((response) => {
+            console.log('Response = ', response);
+          
+            if (response.didCancel) {
+              console.log('User cancelled image picker');
+            } else if (response.error) {
+              console.log('ImagePicker Error: ', response.error);
+            } else if (response.customButton) {
+              console.log('User tapped custom button: ', response.customButton);
+            } else {
+              const source = { uri: response.uri };
+          
+              // You can also display the image using data:
+              // const source = { uri: 'data:image/jpeg;base64,' + response.data };
+          
+              this.setState({
+                avatarSource: source,
+              });
+            }
+          });
+          
+     }
     render(){
         let {navigation} =this.props
+        let {onPress}=this
+        let {avatarSource}=this.state
         return(
          
 
@@ -23,7 +50,9 @@ export default class Profile extends Component{
                 <Content>
                     <Text>Profile</Text>
                     <View style={{justifyContent:'center',alignItems:'center'}}>
-                        <Thumbnail large source={{uri: 'https://vignette.wikia.nocookie.net/yugiohenespanol/images/c/c8/Jaden_Yuki_original.jpg/revision/latest?cb=20160215221041&path-prefix=es'}}/>
+                        <TouchableOpacity onPress={onPress}>
+                            <Thumbnail large source={avatarSource}/>
+                        </TouchableOpacity>
                         <View>
                             <Text>Dylan Torres</Text>
                         </View>
